40e91447774e7d91abf2ec365a494602d9f0e02c
kumaran6777 <kumaran006777@gmail.com>
2022-11-15 16:04:48 +0530

    message

---
+++ src/test/java/org/in/tset.java
@@ -0,0 +1,20 @@
+package org.in;
+
+public class tset {
+private void kumaran() {
+System.out.println("k");
+
+}
+private void suri() {
+	System.out.println("s");
+
+}
+private void mani() {
+	System.out.println("m");
+
+}
+private void srini() {
+	System.out.println("s");
+
+}
+}
